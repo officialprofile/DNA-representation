4a0c7009e50f7f70579666db18d0490a2ce743ab
officialprofile <75314157+officialprofile@users.noreply.github.com>
2021-03-09 12:06:56 +0100

    Add files via upload

--- R/plot2DGraph.R
+++ R/plot2DGraph.R
@@ -3,12 +3,11 @@
 #' @usage plot2DGraph(seq, dim = 2, genbank = FALSE)
 #' @return
 
-plot2DGraph <- function(seq, dim = 2, genbank = FALSE, palette = FALSE,
-                        xlab = 'X', ylab = 'Y', main = ''){
+plot2DGraph <- function(seq, dim = 2, genbank = FALSE, xlab = 'X', ylab = 'Y', main = ''){
   graph <- as.data.frame(dGraph(seq = seq, dim = dim, genbank = genbank)$graph)
 
-  options(scipen = 999)  # Turning-off scientific notation like 1e+48
-  theme_set(theme_classic())
+  ggplot2::options(scipen = 999)  # Turning-off scientific notation like 1e+48
+  ggplot2::theme_set(theme_classic())
 
   gg <- ggplot(graph, aes(x = X, y = Y)) + geom_point(aes(col = freq)) +
     labs(subtitle = ifelse(genbank, seq, ''), y = ylab, x = xlab, title = 'Dynamic graph',
