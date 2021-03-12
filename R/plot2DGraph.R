97ac4118ae1637c06a0c8b9fca76a491feb3156d
officialprofile <75314157+officialprofile@users.noreply.github.com>
2021-03-12 23:46:47 +0100

    Slight graphs polishing

--- R/plot2DGraph.R
+++ R/plot2DGraph.R
@@ -4,7 +4,7 @@
 #' @return
 
 plot2DGraph <- function(seqs, genbank = FALSE, xlab = 'X', ylab = 'Y', main = '',
-                        colorset = c('#ff6361', '#58508d', '#ffa600', '#bc5090', '#003f5c'),
+                        colorset = c('#da186f', '#681f1c', '#ffa600', '#bc5090', '#003f5c'),
                         show.legend = TRUE, legend.pos = 'topleft'){
 
   graphs <- matrix(,0,4)
@@ -15,10 +15,9 @@ plot2DGraph <- function(seqs, genbank = FALSE, xlab = 'X', ylab = 'Y', main = ''
     graphs <- rbind(graphs, cbind(graph, list('nr' = rep(number, nrow(graph)))))
   }
   graphs_shuffled <- graphs[sample(nrow(graphs)), ] # for the overlaps
-  palette <- adjustcolor(colorset[graphs_shuffled$nr], alpha.f = 0.3)
+  palette <- adjustcolor(colorset[graphs_shuffled$nr], alpha.f = 0.2)
 
-  par(family = 'HersheySerif')
-  plot(graphs_shuffled$X, graphs_shuffled$Y, col = palette, pch = 16,
+  plot(graphs_shuffled$X, graphs_shuffled$Y, col = palette, pch = 20,
        cex = sqrt(graphs_shuffled$freq), xlab = xlab, ylab = ylab, main = main)
   if (show.legend){
     legend(legend.pos, legend = seqs, col = colorset[1:length(seqs)], pch=16, pt.cex = 2, cex=1, bty = 'n')
